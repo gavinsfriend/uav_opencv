a2e45254e760308d0c9a1bdb01bc8ee6a57d21da
matthewivan <widjajamatthewivan@gmail.com>
2023-11-20 12:56:51 -0500

    Created code for perspective transformation
    Allows rotation and translation along the x, y, z axis of an image.

---
+++ transform_perspective.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+# Initialize transformation values
+f = 5000
+rotXval = -45
+rotYval = 0
+rotZval = 0
+distXval = 0
+distYval = -2500
+distZval = -2000
+
+if __name__ == '__main__':
+
+    # Read input image, and create output image
+    src = cv2.imread("./images/test_1.jpg")
+    dst = np.zeros_like(src)
+    h, w = src.shape[:2]
+
+    # Initialize transformations
+    rotX = rotXval*np.pi/180
+    rotY = rotYval*np.pi/180
+    rotZ = rotZval*np.pi/180
+    distX = distXval
+    distY = distYval
+    distZ = distZval
+
+    # Camera intrinsic matrix (K)
+    K = np.array([[f, 0, w/2, 0],
+                  [0, f, h/2, 0],
+                  [0, 0,   1, 0]])
+
+    # Find K inverse
+    Kinv = np.zeros((4,3))
+    Kinv[:3,:3] = np.linalg.inv(K[:3,:3])*f
+    Kinv[-1,:] = [0, 0, 1]
+
+    # Rotation matrices around the X,Y,Z axis
+    RX = np.array([[1,           0,            0, 0],
+                   [0,np.cos(rotX),-np.sin(rotX), 0],
+                   [0,np.sin(rotX), np.cos(rotX), 0],
+                   [0,           0,            0, 1]])
+
+    RY = np.array([[ np.cos(rotY), 0, np.sin(rotY), 0],
+                   [            0, 1,            0, 0],
+                   [-np.sin(rotY), 0, np.cos(rotY), 0],
+                   [            0, 0,            0, 1]])
+
+    RZ = np.array([[ np.cos(rotZ),-np.sin(rotZ), 0, 0],
+                   [ np.sin(rotZ), np.cos(rotZ), 0, 0],
+                   [            0,            0, 1, 0],
+                   [            0,            0, 0, 1]])
+
+    # Merge rotation matrices (RX,RY,RZ)
+    R = np.linalg.multi_dot([RX, RY, RZ])
+
+    # Translation matrix
+    T = np.array([[1,0,0,distX],
+                  [0,1,0,distY],
+                  [0,0,1,distZ],
+                  [0,0,0,    1]])
+
+    # Homography matrix (all transformations combined)
+    H = np.linalg.multi_dot([K, R, T, Kinv])
+
+    # Apply matrix transformation
+    cv2.warpPerspective(src, H, (w, h), dst, cv2.INTER_NEAREST, cv2.BORDER_CONSTANT, 0)
+
+    # Save the warped image
+    cv2.imwrite("./images/test_1_warped.jpg", dst)
+
