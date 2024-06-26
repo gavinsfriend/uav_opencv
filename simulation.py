04a92e84ddb91e7cc9c85695105d8968de5301ba
gavinsfriend <cadencebr@gmail.com>
2024-06-25 23:16:05 -0700

    fixed bugs

--- simulation.py
+++ simulation.py
@@ -1,7 +1,6 @@
 import cv2 as cv
-import glob as glob
 import transform_perspective as tp
-import stitching, image
+import image
 
 
 class Simulator:
@@ -23,34 +22,38 @@ def start():
     cv.imshow("original", img.image)
     cv.waitKey(0)
 
-    frame = tp.warp(sim.image.image, sim.rotZ, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
-
     # display image and wait for key
     while True:
+        frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
         cv.imshow("simulation", frame)
-        key = cv.waitKey(0)
+        key = cv.waitKeyEx(0)
+        print(key)
 
         if key == ord("w"):       # tilt up (pitch)
             sim.rotX -= 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
         elif key == ord("s"):     # tilt down (pitch)
             sim.rotX += 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
         elif key == ord("a"):     # pan left (yaw)
-            sim.rotY += 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
-        elif key == ord("d"):     # pan right (yaw)
-            sim.rotY -= 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
-        elif key == ord("q"):     # tilt left (roll)
-            sim.rotZ -= 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
-        elif key == ord("e"):     # tilt right (roll)
             sim.rotZ += 25
-            frame = tp.warp(sim.image.image, sim.rotX, sim.rotY, sim.rotZ, sim.distX, sim.distY, sim.distZ)
-        elif key == ord(" "):     # next stop/frame
-            break
-        elif key == 27:           # (esc) exit simulation
+        elif key == ord("d"):     # pan right (yaw)
+            sim.rotZ -= 25
+        elif key == ord("q"):     # tilt left (roll)
+            sim.rotY -= 25
+        elif key == ord("e"):     # tilt right (roll)
+            sim.rotY += 25
+        elif key == ord("r"):     # ascend
+            sim.distZ += 25
+        elif key == ord("f"):     # descend
+            sim.distZ -= 25
+        elif key == 63234:        # left key
+            sim.distY += 25
+        elif key == 63232:        # up key
+            sim.distX -= 25
+        elif key == 63235:        # right key
+            sim.distY -= 25
+        elif key == 63233:        # down key
+            sim.distX += 25
+        elif key == 27:           # (esc) exit
             break
 
 
